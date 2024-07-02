b70e2a88a05f9fd68da48fa17a716d1872ab0a83
hunrayy <henryokiyi8@gmail.com>
2024-07-02 15:46:04 +0100

    first commit

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "password"
+app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "marvel"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
